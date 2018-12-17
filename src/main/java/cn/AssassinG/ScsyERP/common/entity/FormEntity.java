ff09a808e1a0003a6b0c3209453565c1f030e33a
AssassinG <hangq_zju@hotmail.com>
2018-12-17 19:33:45 +0800

    commit

--- src/main/java/cn/AssassinG/ScsyERP/common/entity/FormEntity.java
+++ src/main/java/cn/AssassinG/ScsyERP/common/entity/FormEntity.java
@@ -1,6 +1,7 @@
 package cn.AssassinG.ScsyERP.common.entity;
 
 import cn.AssassinG.ScsyERP.common.annitations.Valid;
+import com.alibaba.fastjson.annotation.JSONField;
 
 public abstract class FormEntity extends BaseEntity {
     @Valid(varType = Valid.VarType.Other)
@@ -11,11 +12,31 @@ public abstract class FormEntity extends BaseEntity {
         this.IfCompleted = false;
     }
 
+    @JSONField(serialize = false)
     public Boolean getIfCompleted() {
         return IfCompleted;
     }
 
+    @JSONField(deserialize = false)
     public void setIfCompleted(Boolean ifCompleted) {
         IfCompleted = ifCompleted;
+    }
+
+    @JSONField(name = "ifCompleted")
+    public String getIfCompletedNum(){
+        if(IfCompleted)
+            return "0";
+        else
+            return "1";
+    }
+
+    @JSONField(name = "ifCompleted")
+    public void setIfCompletedNum(String str){
+        if(str.equals("0"))
+            this.IfCompleted = true;
+        else if(str.equals("1"))
+            this.IfCompleted = false;
+        else
+            this.IfCompleted = false;
     }
 }
